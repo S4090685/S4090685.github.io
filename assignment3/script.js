7d0f0c37f8a3910994e44dafc98b0177fdbd2f8a
S4090685 <167728609+S4090685@users.noreply.github.com>
2025-05-29 19:58:34 +1000

    cmt

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,6 +1,9 @@
-
-const cards = document.querySelectorAll('.card'); // Get all cards
-console.log(cards);
+// the js is the same for each level.
+// the function of the js is when you click on the card it will flip around and automatically flip back after 2s. But it will only automatically 
+// flip back when two different cards are flipped around. when same images are flipped, the card stays as card front. only 2 card can be clicked
+// at a time
+const cards = document.querySelectorAll('.card');
+console.log(cards); 
 let flipped = []; 
 let locked = false;
 
